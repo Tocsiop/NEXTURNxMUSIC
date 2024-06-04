7929d45a2d4d80560a8b0c1dbd2ae92a578670b4
TOCSI <163741872+Tocsiop@users.noreply.github.com>
2024-06-04 13:25:16 +0530

    Add files via upload

---
+++ NEXTURNxMUSIC/utils/inline/start.py
@@ -0,0 +1,28 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from NEXTURNxMUSIC import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+    ]
+    return buttons
